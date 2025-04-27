699f1be60c0eecee40764532fb2ac27ea452ce1a
kylymretskiy <kylaorozaliev@gmail.com>
2025-04-27 21:05:22 +0600

    HW2

--- product/models.py
+++ product/models.py
@@ -11,12 +11,16 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
     def __str__(self):
         return self.title
 
+STARS = [(i, str(i)) for i in range(1,6)]
 class Review(models.Model):
     text = models.TextField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    stars = models.IntegerField(choices=STARS , default=5)
 
     def __str__(self):
         return f"Review for {self.product.title}"
+
--- product/serializers.py
+++ product/serializers.py
@@ -2,16 +2,33 @@ from rest_framework import serializers
 from .models import Category, Product , Review
 
 class CategorySerializer(serializers.ModelSerializer):
+    products_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = 'id name products_count'.split()
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
+    def get_products_count(self, obj):
+        return obj.products.count()
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True)
+    average_rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = 'id title description price category reviews average_rating'.split()
+
+    def get_average_rating(self, Product):
+        reviews = Product.reviews.all()
+        if reviews:
+            sum_reviews = sum([review.stars for review in reviews])
+            average = sum_reviews / len(reviews)
+            return average
+        return None
